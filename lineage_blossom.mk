3eb556219d316ede8095943177117bcab6841488
Thiago <thjr5965@gmail.com>
2025-07-06 21:39:59 -0300

    blossomy: remove blur lmao
    bro is a memetek g25 lamoakajsjs

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -12,15 +12,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-scr_resolution := 720
-TARGET_SUPPORTS_BLUR := true
-TARGET_ENABLE_BLUR := true
+# Flags
+TARGET_ENABLE_BLUR := false
+TARGET_SUPPORTS_BLUR := false
 TARGET_BOOT_ANIMATION_RES := 720
 
-PRODUCT_NAME := lineage_blossom
+PRODUCT_NAME := aosp_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
