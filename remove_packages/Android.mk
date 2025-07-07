6d203d31d78208697294c01f9eac3f31732110ef
Thiago <thjr5965@gmail.com>
2025-07-06 21:53:28 -0300

    bringup remove_pcakages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -1,15 +1,28 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := remove_packages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    Chrome \
+    Chrome-Stub \
     Drive \
     Gmail2 \
     Maps \
+    YouTube \
+    YouTubeMusic \
+    ScribePrebuilt \
+    Videos \
+    MeetPrebuilt_20240128 \
+    SwitchAccessPrebuilt \
+    TurboAdapter \
+    talkback \
+    YouTube \
+    GoogleTTS \
     PrebuiltGmail \
-    YouTube
+    GoogleFeedback \
+    TagGoogle \
+    TurboPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
